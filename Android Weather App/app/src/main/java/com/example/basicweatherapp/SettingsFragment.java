6218e22ce98b85dd624a04684ef1f18aa5a6da53
Saksham Saxena <saksham1809@gmail.com>
2020-09-28 02:38:31 -0400

    Added the logic to get the radio switch logic for user setting preference values

--- Android Weather App/app/src/main/java/com/example/basicweatherapp/SettingsFragment.java
+++ Android Weather App/app/src/main/java/com/example/basicweatherapp/SettingsFragment.java
@@ -2,13 +2,22 @@ package com.example.basicweatherapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class SettingsFragment extends Fragment {
+
+    private View mFragmentView;
+    private RadioGroup mRadioGroupUnit;
+    private RadioGroup mRadioGroupTime;
+    private RadioButton mRadioButton;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -28,6 +37,14 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        mFragmentView = inflater.inflate(R.layout.fragment_settings, container, false);
+        return mFragmentView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mRadioGroupUnit = mFragmentView.findViewById(R.id.toggle_switch);
+        mRadioGroupTime = mFragmentView.findViewById(R.id.toggle_switch_2);
     }
 }
